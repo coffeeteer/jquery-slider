40dd0e82a5e8139e8b32f7791fc794a0b689fa81
Coffeeteer <tgrieshiemer@gmail.com>
2016-11-19 13:52:39 -0500

    found the errors and finished the project

--- js/script.js
+++ js/script.js
@@ -13,27 +13,39 @@ $(document).ready(function() {
 	//Show first slide
 	$('.active').show();
 
-	$('#next').on('click', function(){
-		$('.active').removeClass('.active').addClass('oldActive');
+	//next handler
+	$('#next').on('click', nextSlide);
+
+	//prev handler
+	$('#prev').on('click', prevSlide);
+
+	if(autoswitch == true){
+		setInterval( nextSlide, autoswitch_speed);
+	}
+
+	//switch to the next slide
+	function nextSlide(){
+		$('.active').removeClass('active').addClass('oldActive');
 		if($('.oldActive').is(':last-child') ){
-			$('.slide').first().addClass('.active');
+			$('.slide').first().addClass('active');
 		} else {
-			$('.oldActive').next().addClass('.active');
+			$('.oldActive').next().addClass('active');
 		}
-		$('.oldActive').removeClass('.oldActive');
+		$('.oldActive').removeClass('oldActive');
 		$('.slide').fadeOut(speed);
 		$('.active').fadeIn(speed);
-	});
+	}
 
-	$('#previous').on('click', function(){
-		$('.active').removeClass('active').addClass('.oldActive');
+	//switch to prev slide
+	function prevSlide() {
+		$('.active').removeClass('active').addClass('oldActive');
 		if($('.oldActive').is(':first-child') ){
 			$('.slide').last().addClass('active');
 		} else {
-			$('.oldActive').previous().addClass('active');
+			$('.oldActive').prev().addClass('active');
 		}
-		$('.oldActive').removeClass('.oldActive');
+		$('.oldActive').removeClass('oldActive');
 		$('.slide').fadeOut(speed);
 		$('.active').fadeIn(speed);
-	});
+	}
 });
